9018326f5c7cd0ebdb6233e385ffb2c790e40c58
Vipin Kumar <115559351+Vipin8990@users.noreply.github.com>
2022-10-18 18:30:12 +0530

    operators

---
+++ area.py
@@ -0,0 +1,10 @@
+# WAP to calculate area of circle using its circumference
+from multiprocessing.heap import Arena
+
+
+x = float(input("eneter circumference of circle"))
+r = x/3.14*2
+area = 3.14*r*r
+print(area)
+
+
---
+++ si.py
@@ -0,0 +1,7 @@
+# WAP to calculate simple and Amt
+p = float((input("enter principal amt")))
+r = float(input("enter rate:"))
+y = int(input("enter number of years"))
+si = p*r*y/100
+print(si)
+
