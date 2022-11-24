d5382d4515a22048d81c7ba4aaf9a60e3e83a1a6
Vipin Kumar <115559351+Vipin8990@users.noreply.github.com>
2022-11-24 18:38:44 +0530

    game_ex2.py

--- Functions/game_ex1.py
+++ Functions/game_ex1.py
@@ -7,9 +7,19 @@ c = Actor('cookie')
 
 def draw():
     screen.clear()
-    #screen.fill('white') to fill the color in background 
+    #screen.fill('white') to fill the color in background   
     p.draw() # to draw the picture
     c.draw()
+    #print('drawing')
+
+def update():
+    #print('updating')
+    p.x += 1
+    p.angle = -10 
+    if p.x > WIDTH:
+        p.x = 0
+    print(p.x, p.y)
+    
 
 
 pgzrun.go() # press ctrl + j for open the terminal to run the program
